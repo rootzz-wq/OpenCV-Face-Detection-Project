c7bf148a1b42234daa84c533d94bdcf4409c2c1a
zms <123@11>
2025-11-17 15:48:39 +0800

    CMake构建文件

---
+++ build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zms/class/camera_demo.cpp" "/home/zms/class/build/CMakeFiles/demo.dir/camera_demo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/zms/opencv/build"
+  "/home/zms/opencv/include"
+  "/home/zms/opencv/modules/core/include"
+  "/home/zms/opencv/modules/flann/include"
+  "/home/zms/opencv/modules/imgproc/include"
+  "/home/zms/opencv/modules/ml/include"
+  "/home/zms/opencv/modules/photo/include"
+  "/home/zms/opencv/modules/dnn/include"
+  "/home/zms/opencv/modules/features2d/include"
+  "/home/zms/opencv/modules/imgcodecs/include"
+  "/home/zms/opencv/modules/videoio/include"
+  "/home/zms/opencv/modules/calib3d/include"
+  "/home/zms/opencv/modules/highgui/include"
+  "/home/zms/opencv/modules/objdetect/include"
+  "/home/zms/opencv/modules/stitching/include"
+  "/home/zms/opencv/modules/ts/include"
+  "/home/zms/opencv/modules/video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
